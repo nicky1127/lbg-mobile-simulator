9a8f5fc50f66dcfea901e367054ffd3f77784a75
Nicky Lai <nickylai@Nickys-MacBook-Air.local>
2024-12-01 15:07:39 +0000

    Added pwd

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import ScreenContainer from "./components/ScreenContainer";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-ltbGreen">
+    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen md:p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-ltbGreen">
       <ScreenContainer />
     </div>
   );
